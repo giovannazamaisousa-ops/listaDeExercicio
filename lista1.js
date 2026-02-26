287b469cda8bcab5f22f5874aadf115cb8e92f92
giovannazs <giovanna.zamai.sousa@escola.pr.gov.br>
2026-02-26 14:29:32 +0000

    resolução da lista

---
+++ lista1.js
@@ -0,0 +1,40 @@
+//Criar um programa para exibir a mensagem: "Olá, Mundo."
+
+let mensagem = 'Olá, Mundo.';
+console.log (mensagem);
+
+//Criar um programa para exibir seu nome completo.
+
+let nome = 'Giovanna Zamai';
+console.log (nome);
+
+//Criar um programa que mostre um número e exiba seu dobro.
+
+let numero = 4;
+let dobro = numero * 2;
+console.log (dobro);
+ 
+
+//Criar um programa que mostre em número e exiba seu triplo.
+
+let num = 60;
+let triplo = numero * 3;
+console.log (triplo);
+
+//Criar um programa que mostre duas notas e calcule a média.
+
+let nota = 20;
+let nota1 = 60;
+let media = nota + nota1 /2
+//Criar um programa para converter metros em centímetros.
+//Criar um programa para mostrar o antecessor e sucessor de um número.
+//Criar um programa que mostre a área de um retângulo.
+//Criar um programa para calcular um aumento de 10% num sálario de 2000,00.
+//Criar um programa para verificar se seu número é par ou impar.
+
+let numero7 = 5;
+if(numero7 % 2 === 0) {
+    console.log('numero par');
+} else {
+    console.log('numero impar');
+}
