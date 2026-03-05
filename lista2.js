1697633f97d67753d8651bfa23051e27f2f4a624
giovannazs <giovanna.zamai.sousa@escola.pr.gov.br>
2026-03-05 14:13:01 +0000

    lista 2

--- lista2.js
+++ lista2.js
@@ -4,19 +4,20 @@ let num = 20;
 if (num > 0) {
     console.log (`O número ${num} é positivo`)
 } else {
-    console.log (`O número ${num} é positivo`)
+    console.log (`O número ${num} é negativo`)
 }
 
 //Criar um programa que mostre se um ano é bissexto.
 
 let ano = 2002;
-if (nao % 4 === 0) {
+if (ano % 4 === 0) {
     console.log ('É bissexto.')
 } else {
     console.log ('Não é bissexto.');
 }
 
-//Criar um programa que mostre uma senha e a mensagem Acesso permitido.
+
+ //Criar um programa que mostre uma senha e a mensagem Acesso permitido.
 
 let senha = "amoeba123"
 if (senha === "amoeba123") {
@@ -26,6 +27,47 @@ if (senha === "amoeba123") {
 }
 
 //Criar um programa que mostre um desconto de 5% para compras maiores de R$100.
+
+let preço2 = 100;
+let desconto2 = preço2 * 0.12;
+if (preço2 >= 100) {
+    console.log(`o valor do desconto é ${desconto2}`)
+} else {
+    console.log ("não tem desoconto")
+}
 //Criar um programa que que classifique uma pessoa como criança, adolescente ou adulto de acordo com a idade.
 //Criar um programa que mostre qual tipo de triângulo de acordo com seus lados (equilatero, isóceles e escalneo).
 //Criar um programa que mostre conceitos A B C ou D e acordo com as notas: >9.5 A; Entre 9.4 e 8.5 B; Entre 8.4 e 7.0 C; <6.9 D.
+
+//Criar um programa para verificar se um número 12 é divisível por 3.
+ let num2 = 12;
+ if (num2 % 2 === 0 && num2 % 3 === 0) {
+    console.log('é divisível por 2 e 3')
+ } else {
+    console.log ('não é divisível por 2 e 3')
+ }
+
+//Criar um programa para mostrar um desconto de 12% para produtos acima de R$80.
+
+let preço = 700;
+let desconto = preço * 0.88;
+if (preço >= 800) {
+    console.log(`o valor do desconto é ${desconto}`)
+} else {
+    console.log ("não tem desoconto")
+}
+//Criar um programa que mostre os números de 1 a 10 usando laço de repetição.
+ 
+for (let i=1;i<=10;i++){
+    console.log(i)
+}
+//Criar um programa para mostrar apenas os números pares entre 1 e 20.
+
+
+//Criar um programa para mostrar apenas os números impares entre 1 e 20.
+//Criar um programa para mostrar a tabuada do 5.
+//Criar um programa para mostrar os números de 1 a 7 usando while.
+//Criar um programa para mostrar  a tabuada do 3 usando while.
+//Criar um programa para mostrar o fatorial de 5.
+//Criar um programa para saber se o número 7 é primário.
+//Criar um programa para calcular a potência de
